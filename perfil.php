4652de55372741defa774126881958616ace719c
Mauricio <araguezmauricio@hotmail.com>
2019-11-13 17:42:56 -0300

    ejemplo de publicaciones

--- perfil.php
+++ perfil.php
@@ -77,6 +77,21 @@
       </section>
     </div>
     <br><br>
+
+    <!-- ejemplo publicaciones -->
+    <div class="container">
+      <section style="border: solid 1px black;" class = "col-lg-12 col-sm-12  ">
+          <img class="float-left" src="img\gato.png" alt="">
+          <div class="d-flex align-items-end flex-column bd-highlight mb-3" style="height: 200px;">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div  class="">
+              <a href="#" style="background-color:#464655; color:white" class="btn mt-2 ml-3">editar</a>
+              <a href="#" style="background-color:#464655; color:white" class="btn mt-2 ml-3">eliminar</a>
+            </div>
+          </div>
+    </div>
+      </section>
+    </div>
   </body>
   <!-- Pie de pagina -->
   <?php incluir_template("footer") ?>
