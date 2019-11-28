eb8c7c351bcca7086c891f90f859b39155d734d7
Mauricio <araguezmauricio@hotmail.com>
2019-11-28 15:31:31 -0300

    publiciones , comentarios , respuestas , modificacion de foto de perfil y banner

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -10,23 +10,22 @@
 
   @section('content')
     <!-- foto perfil -->
-    <div class="container">
-      <section class ="modal-dialog text-center">
-        <article class="col-8 col-md-12 col-sm-8">
 
-          <div class="card w-100 mt-5 ml-auto">
-            <div class="card-body w-100">
-                  <img id="mi-foto" class="col-md-4 col-sm-9" <img src="php/subidas/fotoPerfil.jpg">
-              <p class = "ml-4"><b></b></p>
-              <form class="col-lg-12 col-md-6" action="php/subirFotos.php" method="post" enctype="multipart/form-data">
+    <div id=banner class="">
+    <i id=camarita class="fas fa-camera-retro"></i>
+      <section id=perfil class ="modal-dialog text-center">
+        <article class="col-12 col-md-12 col-sm-12">
+            <div class=" card-body w-100">
+                  <img id="mi-foto" style="margin-left:25%" class="col-5 col-md-6 col-sm-6" <img src="gato.png">
+                  <p class = "ml-4"><b></b></p>
+                  <form class="col-lg-12 col-md-4" action="php/subirFotos.php" method="post" enctype="multipart/form-data">
                 <input class = "form-control-file" type="file" name="archivo" id="archivo"><br>
                 <input class = "btn" style="background-color:#464655; color:white" type="submit" value="Subir foto perfil">
-              </form>
+                </form>
             </div>
           </div>
         </article>
-        </section
-
+      </section>
         <!-- Subida de publicaciones -->
         <div class="container">
           <section class = "col-lg-12 col-sm-12">
@@ -43,20 +42,43 @@
             </div>
           </section>
         </div>
+        <br>
 
         <!-- Publicaciones -->
+
         <div class="container">
-          <section class = "col-lg-12 col-sm-12 mb-4">
-              <div class="d-flex align-items-end flex-column bd-highlight mb-3 card w-100" style="height: 200px;">
-                <div class="card-body w-100">
-                    <img class="float-left" src="php" alt="">
-                  <form class="" action="php/editar.php" method="post">
-                    <input type="hidden" name="postId" value="">
-                    <button class="btn mt-2 ml-3" style="background-color:#464655; color:white" type="submit" name="editar">Editar</button>
-                    <button class="btn mt-2 ml-3" style="background-color:#464655; color:white" type="submit" name="eliminar">Eliminar</button>
-                  </form>
+          <section class = "col-lg-12 col-sm-12">
+            <div class="card w-100 ">
+              <div style="" class="col-lg-12 card-body w-100 ">
+                  <img class="float-left" src="gato.png" alt="">
+                  <br>
+                  <textarea style="margin-top:30px" class="col-sm-6 col-md-8 col-lg-9" mt-2 row="50" name="text" rows="1">Esta es una publicacion</textarea>
+                  <br>
+                  <button class="btn mt-2 ml-3" style="background-color:#464655; color:white" type="submit" name="editar">Editar</button>
+                  <button class="btn mt-2 ml-3" style="background-color:#464655; color:white" type="submit" name="eliminar">Eliminar</button>
+                  <button class="btn mt-2 ml-3" style="background-color:#464655; color:white" type="submit" name="editar">comentar</button>
+
+        <!-- comentarios -->
+            <div class="card w-100 ">
+              <div style="" class="card-body w-100 col-6ds ">
+                <img style="border-radius:50%" class="float-left " src="PERFIL-COMENTARIO.jpg" alt="">
+                  <br>
+                <textarea style="margin-top:30px" class="col-sm-6 col-md-8 col-lg-9" mt-2 row="50" name="text" rows="1">"NOMBRE DEL USUARIO" = Esto es un comentario...</textarea><br>
+                <button class="btn mt-2 ml-3" style="background-color:#464655; color:white ; margin-bottom:35px" type="submit" name="editar">responder</button>
+
+          <!-- respuestas -->
+                <div class="col-10 col-md-10 col-sm-10 container">
+                  <img style="border-radius:50%" class="float-left" src="gato.png" alt="">
+                  <textarea style="margin-top:55px" class="col-sm-6 col-md-8 col-lg-9" mt-2 row="50" name="text" rows="1">" NOMBRE DEL USUARIO" = Esta es una repuesta...</textarea>
+                    <button class="btn mt-2 ml-3" style="background-color:#464655; color:white ; margin-bottom:35px" type="submit" name="editar">responder</button>
                 </div>
-              </div>
-          </div>
-        </section>
+
+                  <br>
+          </section>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
   @endsection
